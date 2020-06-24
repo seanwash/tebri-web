7f877821f153faf7f576abd7022c9aec2788b9ac
Sean Washington <hello@seanwash.com>
2020-06-23 20:00:31 -0700

    Prettier

--- src/app/store.js
+++ src/app/store.js
@@ -1,4 +1,4 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore } from "@reduxjs/toolkit";
 import itemReducer from "../features/items/itemSlice";
 import collectedItemReducer from "../features/collected-items/collectedItemSlice";
 import categoryReducer from "../features/categories/categorySlice";
@@ -7,6 +7,6 @@ export default configureStore({
   reducer: {
     items: itemReducer,
     collectedItems: collectedItemReducer,
-    categories: categoryReducer
+    categories: categoryReducer,
   },
 });
